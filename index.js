cd1112d36e6be5fe8945f40cd9cfdb509af340a0
Toirjon-khaydarov <khaydarov_0507@mail.ru>
2024-09-18 02:11:38 +0500

    figmani tugatdim

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1-misol
+
+// let a=prompt("a ni kiriting:");
+// console.log(typeof a);
+// let p=4 * a;
+// alert (p);
+
+
+// 2-misol
+
+// let a=prompt("a sonini kiriting");
+// let s=a * a;
+// alert (s);
+
+// 3-misol
+
+// let a=prompt("a ni kiriting:");
+// let b=prompt("b ni kiriting:");
+// let s=a * b;
+// alert (s);
+// let p= 2 * s;
+// alert (p);
+
+// 4-misol
+
+// let p=3.14;
+// let d=prompt("d ni kiriting");
+// let l=d * p;
+// alert (l)
+
+// 5-misol
+
+let a=prompt("a_kub sonini kubini topish uchun a ga qiymat kiriting:");
+let a_kub=a * a * a;
+
+let a_kvadrat = a * a;
+alert (a_kub);
+alert (s=6 * a_kvadrat)
+
